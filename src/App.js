2769b3a210760c3cc757022955ae734d30c96a07
Cuma KAPLAN <ccumakaplan@gmail.com>
2023-11-10 16:56:13 +0300

    app.js updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CUMA zeyno abc
+          CUMA zeyno 
         </a>
       </header>
     </div>
