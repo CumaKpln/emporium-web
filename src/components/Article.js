dc513a94f99b2e06833ed2c7897822b93a540943
Cuma KAPLAN <ccumakaplan@gmail.com>
2023-11-14 14:47:09 +0300

    articel
    articel

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,34 +4,39 @@ import Ürünler from "../ürünler";
 
 const Article = () => {
   return (
-    <div className="article p-4"  style={{ maxHeight: "60vh" }}>
+    <div className="article p-4" style={{ maxHeight: "70vh" }}>
       <div className="container-fluid">
         <h4 className="p-1">Beğenebilecekleriniz</h4>
         <div className="row">
-          
-          <div className=" col-sm-2 ">
-            
-            <Ürünler /> <Ürünler /> <Ürünler />
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <Ürünler />
+            <Ürünler />
+            <Ürünler />
           </div>
-          <div className=" col-sm-2">
-            {""}
-            <Ürünler /> <Ürünler /> <Ürünler />
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <Ürünler />
+            <Ürünler />
+            <Ürünler />
           </div>
-          <div className=" col-sm-2 ">
-            {" "}
-            <Ürünler /> <Ürünler /> <Ürünler />
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <Ürünler />
+            <Ürünler />
+            <Ürünler />
           </div>
-          <div className=" col-sm-2">
-            {" "}
-            <Ürünler /> <Ürünler /> <Ürünler />
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <Ürünler />
+            <Ürünler />
+            <Ürünler />
           </div>
-          <div className="col-sm-2">
-            {" "}
-            <Ürünler /> <Ürünler /> <Ürünler />
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <Ürünler />
+            <Ürünler />
+            <Ürünler />
           </div>
-          <div className="col-sm-2 ">
-            {" "}
-            <Ürünler /> <Ürünler /> <Ürünler />
+          <div className="col-lg-2 col-md-4 col-sm-6">
+            <Ürünler />
+            <Ürünler />
+            <Ürünler />
           </div>
         </div>
       </div>
@@ -39,4 +44,5 @@ const Article = () => {
   );
 };
 
-export default Article;
+export default Article
+
