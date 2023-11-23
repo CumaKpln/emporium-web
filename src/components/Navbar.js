65fd152792b647a1ac41716d56fffa8e5799ec8a
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-11-23 11:41:50 +0300

    FilterByName Function Implemented
    En:
    -nameFilter field and FilterByName function began implementation.
    Tr:
    -nameFilter alanı ve FilterByName fonksiyonu uygulanmaya başlandı.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 import "../Styles/navbar.css";
 import { Link } from "react-router-dom";
+
+let nameFilter = useState("");
+
+function FilterByName({ name }) {
+  if (nameFilter === null || nameFilter === "") return true;
+  else return name.contains(nameFilter);
+}
 
 const Navbar = () => {
   return (
@@ -35,38 +42,38 @@ const Navbar = () => {
           <div className="right-navbar">
             {" "}
             <Link to="/hesabım" >
-                <div className="account">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                fill="currentColor"
-                className="bi bi-person-circle"
-                viewBox="0 0 16 16"
-              >
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                <path
-                  fillRule="evenodd"
-                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                />
-              </svg>
-              <div className="hesap"> Hesap </div>
-              {/* buraya kullanıcı username gelecek */}
-            </div>
+              <div className="account">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  className="bi bi-person-circle"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                  />
+                </svg>
+                <div className="hesap"> Hesap </div>
+                {/* buraya kullanıcı username gelecek */}
+              </div>
             </Link>
             <Link to="/UrunDetay">
-            <div className="fav-icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                fill="currentColor"
-                className="bi bi-star-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-              </svg>
-            </div>
+              <div className="fav-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  className="bi bi-star-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+              </div>
             </Link>
             <Link to="/ilan-ver">
               {" "}
