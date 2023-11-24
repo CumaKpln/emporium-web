3a46b94d9ac18a82b167377c9f115ccc1a0171e1
Cuma KAPLAN <ccumakaplan@gmail.com>
2023-11-24 16:48:40 +0300

    s

--- src/components/ürünler.js
+++ src/components/ürünler.js
@@ -6,7 +6,7 @@ function Ürünler() {
   return (
     <div className="products">
       {data["ilan-ver"].map((product, id) => (
-        <div className="img" style={{ cursor: "pointer" }}>
+        <div className="img" key={id} style={{ cursor: "pointer" }}>
           <img src={product.selectedFiles[0].url} alt="foto" />
           <p className="title">{product.title}</p>
         </div>
