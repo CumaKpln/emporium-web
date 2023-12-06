828d1aece2348cf8e5fcec7458758eb662f73374
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-12-06 12:28:32 +0300

    Working On Brand Filter.

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,7 +1,21 @@
+import { filters } from "./Filtering";
 const brands = ["Volkwagen", "Opel", "Séat", "Renault"];
 function BrandSection({ brands }) {
     return <div>
-        {brands.map((b) => (<div>{b}</div>))}
+        {brands.map((b) => (<div><input type="checkbox" onInput={(e) => {
+            if (filters.brands.includes(b)) {
+                const temp = [];
+                filters.brands.forEach((brand) => {
+                    if (brand !== b) {
+                        temp.push(brand);
+                    }
+                });
+                filters.brands = temp;
+            }
+            else {
+                filters.brands.push(b);
+            }
+        }} />{b}</div>))}
     </div>
 }
 function FilterBar() {
