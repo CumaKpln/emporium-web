b412c4fb4723844ac2dd7dea99bdc2c1dce45a26
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2023-12-18 10:10:08 +0300

    Update On Filters

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,30 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import data from "../data/db.json";
 import "../Styles/Product.css";
 import { Link } from "react-router-dom";
-import { filters } from './FilterPage/FilterBar';
 import { selectProduct } from "../control/slices/productSlice"; // Redux reducer'ınızdan gelen action
 
+const brands = ["Volkswagen", "Opel", "Séat", "Renault"];
+const provinces = ["Ankara", "Antalya", "İstanbul", "İzmir", "Aydın", "Mardin", "Diyarbakır", "Yozgat", "Erzurum"];
+
 function Products() {
+  const [MinP, setMinP] = useState(0);
+  const [MaxP, setMaxP] = useState(1000000000);
+  const [NameF, setNameF] = useState("");
+  const [BrandF, setBrandF] = useState([]);
+  const [ProvinceF, setProvinceF] = useState("");
+  const [DistrictF, setDistrictF] = useState("");
+
+  const NameSection = () => {
+    const handleNameChange = (e) => {
+      setNameF(e.target.value);
+    };
+
+    return <div key={"name"} id="name">
+      {NameF}
+      <input type="text" onInput={handleNameChange} />
+    </div>
+  }
+
+  const BrandSection = () => {
+    return <div key={"brands"}>
+      {brands.map((b) => (<div key={b}><input type="checkbox" onInput={(e) => {
+        if (BrandF.includes(b)) {
+          const temp = [];
+          BrandF.forEach((brand) => {
+            if (brand !== b) {
+              temp.push(brand);
+            }
+          });
+          setBrandF(temp);
+        }
+        else {
+          setBrandF([...BrandF, b]);
+        }
+      }} />{b}</div>))}
+    </div>
+  }
+
+  const PriceSection = () => {
+    return <div key={"price"}>
+      <input key="minp" type="number" onInput={(e) => (setMinP(parseInt(e.target.value)))} />
+      <input key="maxp" type="number" onInput={(e) => (setMaxP(parseInt(e.target.value)))} />
+    </div>
+  }
+
+  const ProvinceSection = () => {
+    return <div key={"province"}>
+      <select key="province" onInput={(e) => (setProvinceF(e.target.value))}>
+        {provinces.map((p) => (<option key={p} value={p}>{p}</option>))}
+      </select>
+    </div>
+  }
+
+  const FilterBar = ({ category = "" }) => {
+    return <div className="filter-bar">
+      <NameSection />
+      <BrandSection />
+      <PriceSection />
+      <ProvinceSection />
+    </div>
+  }
+
+  const FilterProduct = (product) => {
+    if (product.price < MinP || product.price > MaxP) return false;
+    if (ProvinceF != "" && product.province != ProvinceF) return false;
+    if (DistrictF != "" && product.district != DistrictF) return false;
+    if (BrandF.length != 0 && !BrandF.includes(product.brand)) return false;
+    return true;
+  }
+
   const dispatch = useDispatch();
 
   const selectedProduct = (product) => { // Redux store'a seçilen ürünü gönderme
     dispatch(selectProduct(product));
   };
 
-  
+
+
   return (
-    <div className="products">
-      {data["ilan-ver"].map((product, id) => (
-          <div className="img p-3" key={id} style={{ cursor: "pointer" }}>
-            <Link to={`/urun-detayi/${id}`} onClick={() => selectedProduct(product)}>
-              <img src={product.selectedFiles[0].url} alt="foto" />
-              <p className="title" style={{flex:"1"}}>{product.title}</p>
-            </Link>
-          </div>
-        )
-        )}
+    <div className="products" >
+      <FilterBar /> <br />
+      {data["ilan-ver"].filter((p) => FilterProduct(p)).map((product, id) => (
+        <div className="img p-3" key={id} style={{ cursor: "pointer" }}>
+          <Link to={`/urun-detayi/${id}`} onClick={() => selectedProduct(product)}>
+            <img src={product.selectedFiles[0].url} alt="foto" />
+            <p className="title" style={{ flex: "1" }}>{product.title}</p>
+          </Link>
+        </div>
+      )
+      )}
     </div>
   );
 
