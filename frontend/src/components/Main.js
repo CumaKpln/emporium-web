5cab6da7e50e5474e628718a81eaf4d53a9560ed
Cuma KAPLAN <ccumakaplan@gmail.com>
2024-01-03 16:30:38 +0300

    Revert "Revert "updated""
    This reverts commit 86962c9fe4cfe1639beb7ca27097d6deec2747fc.

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -11,7 +11,7 @@ const Main = () => {
   return (
     <div>
       <Navbar />
-      <div className="main bg-danger">
+      <div className="main">
         <div className="container d-flex">
           <CategoryProvider>
             <ProvinceProvider>
