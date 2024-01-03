0d7535c451085b1a07f2ea4f6eff7bbed0754c11
Tahsin Başar Paksoy <tahsin.basar.paksoy.019@gmail.com>
2024-01-03 13:21:52 +0300

    Bug Fixes About Province Filter

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { CategoryProvider } from "./CategoryContext";
 import { SearchProvider, useSearch } from './SearchContext';
+import { ProvinceProvider } from "./ProvinceContext";
 
 const Main = () => {
   const { nameFilter } = useSearch();
@@ -16,14 +17,16 @@ const Main = () => {
         <div className="main ">
           <div className="container d-flex">
             <CategoryProvider>
-              <div className="col-md-3 col-sm-3 sidebar">
-                <Sidebar />
-              </div>
-              <div className="col-md-9 col-sm-9">
-                {/* Article bileşenini kaldırdık, yerine Products bileşenini ekledik */}
-                <Article nameFilter={nameFilter} />
-                {/* <FilterBar category="asd" /> */}
-              </div>
+              <ProvinceProvider>
+                <div className="col-md-3 col-sm-3 sidebar">
+                  <Sidebar />
+                </div>
+                <div className="col-md-9 col-sm-9">
+                  {/* Article bileşenini kaldırdık, yerine Products bileşenini ekledik */}
+                  <Article nameFilter={nameFilter} />
+                  {/* <FilterBar category="asd" /> */}
+                </div>
+              </ProvinceProvider>
             </CategoryProvider>
           </div>
         </div>
